6342772e3220e5d039e9cd4567d79f937efe84c2
Radwa-Adell <elshorbagyradwa81@gmail.com>
2024-11-29 02:01:27 -0800

    Added Files

---
+++ test.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0'; 
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is the first page!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, HOST, () => {
+  console.log(`Application started successfully !`);
+});
